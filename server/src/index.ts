284cef0dc45b04df431f117f53c3d9e34f454323
yo1 <yo1.cvetkov@gmail.com>
2024-09-25 16:17:16 +0200

    Server modifications

--- server/src/index.ts
+++ server/src/index.ts
@@ -40,8 +40,8 @@ app.use("/teams", teamsRoutes);
 
 // Server
 
-const port = process.env.PORT || 3000;
+const port = Number(process.env.PORT) || 3000;
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`Server running on port ${port}`);
 });
