5610ec5854a87786a21ff1a1f21332e41a077711
yo1 <yo1.cvetkov@gmail.com>
2024-09-26 21:41:01 +0200

    Added authentication cognito

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -3,6 +3,7 @@
 import { useEffect } from 'react';
 import Navbar from '../components/Navbar';
 import Sidebar from '../components/Sidebar';
+import AuthProvider from './authProvider';
 import StoreProvider, { useAppSelector } from './redux';
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
@@ -36,7 +37,9 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
 const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <StoreProvider>
-      <DashboardLayout>{children}</DashboardLayout>
+      <AuthProvider>
+        <DashboardLayout>{children}</DashboardLayout>
+      </AuthProvider>
     </StoreProvider>
   );
 };
