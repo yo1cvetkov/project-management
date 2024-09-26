d05c7d9e0f98744b0ad226f2b4b1a48751582d6e
yo1 <yo1.cvetkov@gmail.com>
2024-09-26 21:48:49 +0200

    Create user route post trigger

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { createTask, getTasks, updateTaskStatus } from "../controllers/taskController";
-import { getUsers } from "../controllers/userController";
+import { getUsers, postUser } from "../controllers/userController";
 
 const router = Router();
 
 router.get("/", getUsers);
+router.post("/", postUser);
 
 export default router;
